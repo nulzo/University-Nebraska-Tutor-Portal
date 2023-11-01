984f46ead227a377f5f3d779925a263db3394d08
Nolan Gregory <65730528+nulzo@users.noreply.github.com>
2023-11-01 01:40:58 -0500

    Changed the default header text

--- frontend-ts/src/components/typography/Header.tsx
+++ frontend-ts/src/components/typography/Header.tsx
@@ -1,27 +1,16 @@
-import { Component } from "react";
-
-type PropType = {
-  text?: string;
-  textcolor?: string;
-  textweight?: string;
-  subtext?: any;
-  subtextcolor?: string;
-  subtextweight?: string;
-};
-
-export default class Header extends Component<PropType> {
-  render() {
-    return (
-      <div className="mt-8 flex text-center pb-6 sm:text-start sm:block justify-center text-foreground dark:text-foreground">
+export default function Header({ text, subtext }: { text: string, subtext: string }) {
+  return (
+    <div className="mt-8 flex text-center pb-4 sm:text-start sm:block justify-center text-foreground dark:text-foreground">
+      <div className="flex flex-col">
+        <h2 className="text-2xl font-bold tracking-tight text-foregound">
+          {text}
+        </h2>
         <div className="flex flex-col gap-3">
-          <h1 className="text-3xl font-semibold">{this.props.text!}</h1>
-          <h2>
-            <span className="items-center text-foreground font-lighter dark:text-foreground">
-              {this.props.subtext!}
-            </span>
-          </h2>
+          <p className="text-muted-foreground">
+            {subtext!}
+          </p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
